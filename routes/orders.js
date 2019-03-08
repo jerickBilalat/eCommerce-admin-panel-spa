4439a58a006fb4b10671f702e38b5a8b79ba1eb4
Jerick Bilalat <jrk.mb.xm@gmail.com>
2019-03-08 11:11:29 -0600

    add next parmas to order routes

--- routes/orders.js
+++ routes/orders.js
@@ -4,43 +4,43 @@ const express = require('express');
 const router = express.Router();
 
 // read all orders by name desc
-router.get("/", (req,res) => {
+router.get("/", (req,res, next) => {
   Order.find()
     .sort("name")
     .exec(function(err, order) {
-      if(err) return new Error(err);
+      if(err) return next(err);
       res.send(order);
     })
 })
 
 // read one order by id
-router.get('/:id',  (req, res) => {
+router.get('/:id',  (req, res, next) => {
   Order.findById(req.params.id, function (err, order) {
-    if(err) return new Error(err);
+    if(err) return next(err);
     res.send(order);
    });
 })
 
 // create order
-router.post('/create_order', (req, res) => {
+router.post('/create_order', (req, res, next) => {
   Order.create(req.body, function(err, order) {
-    if(err) return new Error(err);
+    if(err) return next(err);
     res.send(order);
   })
 })
 
 // update order
-router.put("/edit_order/:id", (req, res) => {
+router.put("/edit_order/:id", (req, res, next) => {
   Order.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, order) {
-    if(err) return new Error(err);
+    if(err) return next(err);
     res.send(order);
   });
 });
 
 // delete order
-router.delete("/delete_order/:id", (req, res) => {
+router.delete("/delete_order/:id", (req, res, next) => {
   Order.findByIdAndRemove(req.params.id, function(err, order) {
-    if(err) return new Error(err);
+    if(err) return next(err);
     res.send(order);
   })
 });
